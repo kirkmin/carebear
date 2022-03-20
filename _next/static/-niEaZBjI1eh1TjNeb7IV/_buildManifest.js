3c6be1cdecfe56239bb166fbbc47363acae24dc1
kirkmin <10121124+kirkmin@users.noreply.github.com>
2022-03-20 19:15:44 +0000

    Deploying to gh-pages from @ kirkmin/carebear@bc5c885e76ecc2ff5c919deb4a62391899bc92ba 🚀

---
+++ _next/static/-niEaZBjI1eh1TjNeb7IV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4aed69abe28fe336.css","static/chunks/pages/index-dd98bb2f500cf905.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
