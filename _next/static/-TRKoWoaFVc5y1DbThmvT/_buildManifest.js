41f1c68d39cbe89fc51dce04e7b7a8fadd3b09d0
kirkmin <10121124+kirkmin@users.noreply.github.com>
2022-03-21 04:13:26 +0000

    Deploying to gh-pages from @ kirkmin/carebear@f6435c5ea2dfa59073524186e07ebfed2bf95eec 🚀

---
+++ _next/static/-TRKoWoaFVc5y1DbThmvT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/197162610250d663.css","static/chunks/pages/index-dd98bb2f500cf905.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/-niEaZBjI1eh1TjNeb7IV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4aed69abe28fe336.css","static/chunks/pages/index-dd98bb2f500cf905.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
